95d2f7431db6bbabead3d43dafd06c39d91a24ec
MarcosStark <88205157+MarcosStark@users.noreply.github.com>
2023-07-25 22:55:14 -0300

    Update calculatorCalc.js

--- ownerproject - calculator/scripts/calculatorCalc.js
+++ ownerproject - calculator/scripts/calculatorCalc.js
@@ -14,8 +14,17 @@ class calculatorClass{
 
             switch(e.key){
 
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
                 case "5":
-                    console.log("cinco");
+                case "6":
+                case "7":
+                case "8":
+                case "9":   
+                    console.log("Some number!");
                     break;
             }
         })
